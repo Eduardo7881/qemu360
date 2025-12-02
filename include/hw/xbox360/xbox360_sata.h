964ed75c490846af37f167732c3a53670950ec82
EduardoPlayss121 <166447787+Eduardo7881@users.noreply.github.com>
2025-12-02 17:40:17 -0300

    Create xbox360_sata.h

---
+++ include/hw/xbox360/xbox360_sata.h
@@ -0,0 +1,29 @@
+#ifndef HW_XBOX360_SATA_H
+#define HW_XBOX360_SATA_H
+
+#include "hw/sysbus.h"
+#include "qom/object.h"
+#include "sysemu/block-backend.h"
+
+#define TYPE_XENON_SATA "xenon.sata"
+OBJECT_DECLARE_SIMPLE_TYPE(XenonSATAState, XENON_SATA)
+
+struct XenonSATAState {
+    SysBusDevice parent_obj;
+    MemoryRegion iomem;
+    qemu_irq irq;
+};
+
+/* Public functions */
+XenonSATAState *xenon_sata_create(MemoryRegion *parent, hwaddr base);
+void xenon_sata_attach_drive(XenonSATAState *s, int port, BlockBackend *blk,
+                            const char *serial, const char *model);
+void xenon_sata_dump_state(XenonSATAState *s);
+
+/* SATA Ports */
+enum SATAPorts {
+    SATA_PORT_HDD = 0,
+    SATA_PORT_DVD = 1,
+};
+
+#endif
