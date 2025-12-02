c561dde46ed8ab1ef5dc716db01a74a448ba4211
EduardoPlayss121 <166447787+Eduardo7881@users.noreply.github.com>
2025-12-02 17:18:34 -0300

    Update xbox360_integration.c

--- hw/xbox360/xbox360_integration.c
+++ hw/xbox360/xbox360_integration.c
@@ -4,6 +4,51 @@
 #include "qemu/osdep.h"
 #include "cpu.h"
 
+void xenon_audio_timer_integration_init(XenonState *s) {
+    s->xma = xenon_xma_create(get_system_memory(), 0x80005000);
+    printf("[XMA]   Initialized at 0x80005000\n");
+    
+    s->timer = xenon_timer_create(get_system_memory(), 0x80006000);
+    printf("[TIMER] Initialized at 0x80006000\n");
+    
+    /* Connect XMA to GIC */
+    if (s->xma && s->gic) {
+        qemu_irq xma_irq = xenon_xma_get_irq(s->xma);
+        /* Connect to GIC IRQ 40 (audio) */
+        // TODO
+    }
+    
+    /* Connect timers to GIC */
+    if (s->timer && s->gic) {
+        /* System timer (IRQ 64) */
+        qemu_irq timer0_irq = xenon_timer_get_irq(s->timer, TIMER_SYSTEM);
+        /* GPU timer (IRQ 65) */
+        qemu_irq timer1_irq = xenon_timer_get_irq(s->timer, TIMER_GPU);
+        /* Audio timer (IRQ 66) */
+        qemu_irq timer2_irq = xenon_timer_get_irq(s->timer, TIMER_AUDIO);
+        
+        /* Connect to GIC */
+        // TODO
+    }
+    
+    if (s->gpu && s->timer) {
+        /* Set up GPU timer for 60Hz VSYNC */
+        xenon_timer_set_frequency(s->timer, TIMER_GPU, 60);
+        xenon_timer_start(s->timer, TIMER_GPU);
+    }
+    
+    if (s->xma && s->timer) {
+        /* Set up audio timer for 48kHz */
+        xenon_timer_set_frequency(s->timer, TIMER_AUDIO, 48000);
+        xenon_timer_start(s->timer, TIMER_AUDIO);
+    }
+    
+    if (s->timer) {
+        xenon_timer_set_frequency(s->timer, TIMER_SYSTEM, 1000);
+        xenon_timer_start(s->timer, TIMER_SYSTEM);
+    }
+}
+
 void xenon_pcie_integration_init(XenonState *s) {
     s->pcie = xenon_pcie_create(get_system_memory(), 0x80002000);
     printf("[PCIE] Initialized at 0x80002000\n");
@@ -28,6 +73,34 @@ void xenon_pcie_integration_init(XenonState *s) {
         /* Channel 0 for GPU command DMA */
         xenon_dma_start_transfer(s->dma, DMA_CHANNEL_GPU,
                                 0x80000000, 0xC0000000, 0x10000);
+    }
+}
+
+static void xenon_timer_irq_handler(void *opaque, int timer_num) {
+    XenonState *s = opaque;
+    
+    switch (timer_num) {
+        case TIMER_SYSTEM:
+            /* System tick - update kernel timing */
+            if (s->kernel_state) {
+                xbox360_update_timing(s->kernel_state);
+            }
+            break;
+            
+        case TIMER_GPU:
+            /* GPU VSYNC timer */
+            if (s->gpu) {
+                s->gpu->interrupt_status |= GPU_INTR_VBLANK;
+                xbox360_gpu_update_irq(s->gpu);
+            }
+            break;
+            
+        case TIMER_AUDIO:
+            /* Audio buffer refill timer */
+            if (s->xma) {
+                xenon_xma_buffer_refill(s->xma);
+            }
+            break;
     }
 }
 
@@ -60,6 +133,20 @@ void xenon_connect_interrupts(XenonState *s) {
 /* GPU interrupt handler with GIC routing */
 static void xenon_gpu_interrupt_handler(void *opaque, uint32_t interrupts) {
     XenonState *s = opaque;
+
+    if (interrupts & GPU_INTR_VBLANK) {
+        /* VSYNC triggered by hardware timer */
+        xenon_gic_assert_irq(s->gic, IRQ_GPU_VBLANK, 0);
+        
+        /* Update display via DMA */
+        if (s->dma && s->gpu) {
+            xenon_dma_start_transfer(s->dma, DMA_CHANNEL_GPU,
+                                    s->gpu->display.base_addr,
+                                    0x20000000,
+                                    s->gpu->display.width * 
+                                    s->gpu->display.height * 4);
+        }
+    }
 
     if (interrupts & GPU_INTR_VBLANK) {
         xenon_gic_assert_irq(s->gic, IRQ_GPU_VBLANK, 0);
